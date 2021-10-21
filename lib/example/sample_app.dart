9d3111d8118de312e688a0bd8a788c49cbba7ab0
JERRY <hi.jerry@outlook.com>
2021-10-22 02:38:25 +0800

    feat(materialapp学习): 对MaterialApp 组件widget学习理解

---
+++ lib/example/sample_app.dart
@@ -0,0 +1,55 @@
+import 'package:flustars/flustars.dart';
+import 'package:flutter/material.dart';
+
+class SampleApp extends StatelessWidget {
+  const SampleApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  const SampleAppPage({Key? key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  // Default placeholder text.
+  String textToShow = 'I Like Flutter';
+
+  void _updateText() {
+
+    setState(() {
+      // Update the text.
+      textToShow = 'Flutter is Awesome!';
+      LogUtil.e("点击一下");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample App'),
+      ),
+      body: Center(child: Text(textToShow)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _updateText,
+        tooltip: 'Update Text',
+        child: const Icon(Icons.update),
+      ),
+    );
+  }
+}
+
---
+++ lib/example/update_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UpdateButton extends StatelessWidget {
+  const UpdateButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
