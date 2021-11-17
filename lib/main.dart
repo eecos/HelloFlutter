85899de29d710f24eb03b8034c160082417d345d
jerry <hi.jerry@outlook.com>
2021-11-17 15:28:55 +0800

    feat(animatedcontainer): learn AnimatedContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hello_flutter/example/demo1.dart';
-import 'package:hello_flutter/example/demo4.dart';
-import 'package:hello_flutter/example/demo5.dart';
-import 'package:hello_flutter/example/demo6.dart';
-import 'package:hello_flutter/example/demo7.dart';
-import 'package:hello_flutter/example/official_example.dart';
-
 
 void main() {
   runApp(const RunMyApp());
@@ -19,19 +12,69 @@ class RunMyApp extends StatefulWidget {
 }
 
 class _RunMyAppState extends State<RunMyApp> {
+  var _isStr1 = 'Container';
+
+  var _str;
+  double _width = 300.0;
+  double _height = 200.0;
+
+  bool _isAdd = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (_str == null) {
+      _str = _isStr1;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     /// return const MyApp();
     return MaterialApp(
-      title: 'subtitle_bar_partial_fixed',
+      title: 'demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: ChartApp(),
-      //home: BottomSheetDemo(),
-      //home: DialogPage(),
-      //home: DottomBarWithSheet(),
-      home: WaveDemoApp(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('动画Demo'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _updateStatus,
+          child: Icon(Icons.add_sharp),
+        ),
+        body: Center(
+          child: AnimatedContainer(
+            width: _width,
+            height: _height,
+            color: _isAdd ? Colors.lightBlue : Colors.green,
+            duration: Duration(seconds: 1),
+            child: Center(
+              child: Text(
+                '$_str',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  void _updateStatus() {
+    setState(() {
+      _str = '''
+      width  == $_width
+      height == $_height
+      ''';
+      if (_width == 320) {
+        _isAdd = !_isAdd;
+      } else if (_width == 299) {
+        _isAdd = !_isAdd;
+      }
+      _isAdd ? _width++ : _width--;
+    });
   }
 }
