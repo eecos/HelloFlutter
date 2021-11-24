f921296eedeaa18a45b09001e1d48b01f0af9fd9
jerry <hi.jerry@outlook.com>
2021-11-25 00:45:23 +0800

    feat(重构): 重构仓库

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,115 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const RunMyApp());
+  runApp(const MyApp());
 }
 
-class RunMyApp extends StatefulWidget {
-  const RunMyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
-  _RunMyAppState createState() => _RunMyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
-class _RunMyAppState extends State<RunMyApp> {
-  var _isStr1 = 'Container';
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  var _str;
-  double _width = 300.0;
-  double _height = 200.0;
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
-  bool _isAdd = true;
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    if (_str == null) {
-      _str = _isStr1;
-    }
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    /// return const MyApp();
-    return MaterialApp(
-      title: 'demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('动画Demo'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _updateStatus,
-          child: Icon(Icons.add_sharp),
-        ),
-        body: Center(
-          child: AnimatedContainer(
-            width: _width,
-            height: _height,
-            color: _isAdd ? Colors.lightBlue : Colors.green,
-            duration: Duration(seconds: 1),
-            child: Center(
-              child: Text(
-                '$_str',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  void _updateStatus() {
-    setState(() {
-      _str = '''
-      width  == $_width
-      height == $_height
-      ''';
-      if (_width == 375) {
-        _isAdd = !_isAdd;
-        print('状态改变---$_isAdd');
-      } else if (_width == 275) {
-        _isAdd = !_isAdd;
-        print('状态改变---$_isAdd');
-      }
-      _isAdd ? _width+=25 : _width-=25;
-      _isAdd ? _height+=25 : _height-=25;
-    });
   }
 }
